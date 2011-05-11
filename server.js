c595fa465ce365d887267a9c303a6b50661d9c9f
Franz SEO <franz@Franz-SEOs-MacBook-Pro-2.local>
2011-05-11 13:24:08 +0200

    update

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ var widget = function(spot_id, callback)
    res.send('Hello World');
  });
 
- app.listen(8124, '127.0.0.1');
+app.listen(8124, '127.0.0.1');
  
  var socket = io.listen(app); 
 socket.on('connection', function(client){ 
